05b2de5ef7db0d6ed4f2e5080cb6988dfc23310d
Sarujan28 <sarujan28@hotmail.co.uk>
2023-12-20 16:25:04 +0000

    Adding init_db_engine function that creates engine connected to AWS database

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,5 @@
 import yaml
+from sqlalchemy import create_engine
 
 class DatabaseConnecter:
     def read_db_creds(self):
@@ -7,3 +8,15 @@ class DatabaseConnecter:
         self.credentials = credentials
         
         return credentials
+
+    def init_db_engine(self):
+        self.read_db_creds()
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        RDS_HOST = self.credentials['RDS_HOST']
+        RDS_PASSWORD = self.credentials['RDS_PASSWORD']
+        RDS_USER = self.credentials['RDS_USER']
+        RDS_DATABASE = self.credentials['RDS_DATABASE']
+        RDS_PORT = self.credentials['RDS_PORT']
+        engine = create_engine(f'{DATABASE_TYPE}+{DBAPI}://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}')
+        self.engine = engine
