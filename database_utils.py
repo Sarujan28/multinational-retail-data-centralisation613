f0dcddd848faf946fcb50bc4858478ca1f672325
Sarujan28 <sarujan28@hotmail.co.uk>
2023-12-20 16:36:05 +0000

    Adding upload_to_db function that uploads dataframe to local postgresql

--- database_utils.py
+++ database_utils.py
@@ -27,3 +27,23 @@ class DatabaseConnecter:
         table_names = inspector.get_table_names()
         self.table_names = table_names
         print(table_names)
+
+    def upload_to_db(self, clean_dataframe, sql_table,):
+        from data_cleaning import DataCleaning
+        cleaning = DataCleaning()
+        self.cleaning = cleaning
+        self.clean_dataframe = clean_dataframe
+        table_upload = clean_dataframe
+        self.table_upload = table_upload
+        table_upload.to_csv('final_user.csv')
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = 'localhost'
+        USER = 'postgres'
+        PASSWORD = 'dragon'
+        DATABASE = 'sales_data'
+        PORT = 5432
+        engine_2 = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        self.engine_2 = engine_2
+        print(table_upload)
+        table_upload.to_sql(sql_table, self.engine_2, if_exists = 'replace', index = False)
